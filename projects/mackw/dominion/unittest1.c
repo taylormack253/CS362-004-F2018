d869c34d4948474e06d2f157a9d621846003289c
William Taylor Mack <mackw@oregonstate.edu>
2018-10-17 20:40:33 -0700

    unittest1 fixes

--- projects/mackw/dominion/unittest1.c
+++ projects/mackw/dominion/unittest1.c
@@ -3,9 +3,9 @@
 // Author: William Taylor Mack
 // Description: Unit testing for getCost function.
 
-#include "dominion.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "dominion.h"
 #include "rngs.h"
 
 int cardCosts[] = {0,2,5,8,0,3,6,6,5,4,4,5,4,4,3,4,3,5,3,5,3,4,2,5,4,4,4};
