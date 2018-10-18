51c313389c380f635112605c7986c5cd04b4b6a7
William Taylor Mack <mackw@oregonstate.edu>
2018-10-17 20:37:32 -0700

    unittest1 changes

--- projects/mackw/dominion/unittest1.c
+++ projects/mackw/dominion/unittest1.c
@@ -12,9 +12,12 @@ int cardCosts[] = {0,2,5,8,0,3,6,6,5,4,4,5,4,4,3,4,3,5,3,5,3,4,2,5,4,4,4};
 
 int main () {
 	// create card and initilize to 0
-	int card = 0;
-	int cost = 0;
-	int i= 0;
+	int card 
+	card = 0;
+	int cost 
+	cost = 0;
+	int i
+	i = 0;
 
 	printf ("Unit test 1\n");
 
@@ -24,9 +27,9 @@ int main () {
 		cost = getCost(card);
 		printf("Cost %d coins\n", cost);
 		if (cost = cardCosts[i])
-			printf("Cost is CORRECT");
+			printf("Cost is CORRECT\n");
 		else 
-			printf("Cost is INCORRECT");
+			printf("Cost is INCORRECT\n");
 		i++;
 	}
 	
