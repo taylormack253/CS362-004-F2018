a28851343c0f402f964567ac8f9a2d01dd7f3f09
William Taylor Mack <mackw@oregonstate.edu>
2018-10-19 17:57:12 -0700

    unittest3 changed again

--- projects/mackw/dominion/unittest3.c
+++ projects/mackw/dominion/unittest3.c
@@ -14,6 +14,53 @@
 #define TRUE 1
 #define FALSE 0
 
+void assertCustom(int boolean, char * message){
+
+    if(boolean == TRUE){
+
+        printf("TEST PASSED: %s\n", message );
+    }
+    if(boolean == FALSE){
+
+        printf("TEST FAILED: %s\n", message );
+
+
+    }
+
+}
+
+void assertGameState(int player, struct gameState * oldG, struct gameState * newG){
+    int failed = FALSE;
+    if(oldG->handCount[player] !=   newG->handCount[player]){
+        printf("TEST FAILED: Hand Count for non-action player Changed\n");
+        failed = TRUE;
+    }
+    if(oldG->numPlayers != newG->numPlayers){
+        printf("TEST FAILED: Number of Players in Game Changed\n");
+        failed = TRUE;
+    }
+    if(oldG->deckCount[player] !=   newG->deckCount[player]){
+        printf("TEST FAILED: Deck Count for non-action player Changed\n");
+        failed = TRUE;
+    }
+    int i;
+    for(i = curse; i < NUM_CARDS; i++){
+        if(oldG->supplyCount[i] != newG->supplyCount[i]){
+            printf("TEST FAILED Card %d Supply Count Changed\n", i);
+           failed = TRUE;
+        }
+        if(oldG->embargoTokens[i] != newG->embargoTokens[i]){
+            printf("TEST FAILED Embargo Token on Card %d Changed\n", i);
+            failed = TRUE;
+
+        }
+    }
+    if(!failed){
+        printf("TEST PASSED: Game State Invariants\n");
+    }
+
+}
+
 // Function to test that the game state was not altered by isGameOver
 void gameTests(int thisPlayer,struct gameState G, struct gameState testG){
 
