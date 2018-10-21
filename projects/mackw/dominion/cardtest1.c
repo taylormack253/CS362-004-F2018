902f57453d567434f8f28a05643a96dcc143ac42
William Taylor Mack <mackw@oregonstate.edu>
2018-10-21 11:07:34 -0700

    card test 1 fixed

--- projects/mackw/dominion/cardtest1.c
+++ projects/mackw/dominion/cardtest1.c
@@ -15,15 +15,15 @@
 #define FALSE 0
 #define NUM_CARDS 27
 
-void assertCustom(int boolean, char * message){
+void assertCustom(int boolean, char * passMsg, char * failMsg){
 
     if(boolean == TRUE){
 
-        printf("TEST PASSED: %s\n", message );
+        printf("TEST PASSED: %s\n", passMsg );
     }
     if(boolean == FALSE){
 
-        printf("TEST FAILED: %s\n", message );
+        printf("TEST FAILED: %s\n", failMsg );
 
 
     }
@@ -33,16 +33,16 @@ void assertCustom(int boolean, char * message){
 void gameTests(int player,struct gameState G, struct gameState testG){
     int failed = FALSE;
     int i;
-    assertCustom(testG.handCount[player] == G.handCount[player] + 3 - 1, "Player 1 receives 3 cards");
-    assertCustom(testG.deckCount[player] == G.deckCount[player] - 3, "Cards came from Player 1's deck");
-    assertCustom(testG.handCount[player+1] == G.handCount[player+1], "Player 2 receives no cards");
-    assertCustom(testG.deckCount[player+1] == G.deckCount[player+1], "Player 2 deck has same number of cards");
-    assertCustom(testG.coins == G.coins, "No extra coins received");
-    assertCustom(testG.whoseTurn == G.whoseTurn, "Same Players Turn");
-    assertCustom(testG.numActions == G.numActions, "Number of actions" );
-    assertCustom(testG.numBuys == G.numBuys, "Number of Buys" );
-    assertCustom(testG.playedCardCount == G.playedCardCount, "Number of Cards Discarded");
-    assertCustom(testG.numPlayers == G.numPlayers, "Same number of players in the game");
+    assertCustom(testG.handCount[player] == G.handCount[player] + 3 - 1, "Player 1 receives 3 cards", "Player 1 does not receive 3 cards");
+    assertCustom(testG.deckCount[player] == G.deckCount[player] - 3, "Cards came from Player 1's deck", "Cards DID NOT come from Player 1's deck");
+    assertCustom(testG.handCount[player+1] == G.handCount[player+1], "Player 2 receives no cards", "Player 2 hand count changed");
+    assertCustom(testG.deckCount[player+1] == G.deckCount[player+1], "Player 2 deck has same number of cards", "Player 2 deck count chnaged");
+    assertCustom(testG.coins == G.coins, "No extra coins received","Extra coins recieved");
+    assertCustom(testG.whoseTurn == G.whoseTurn, "Same Players Turn", "NOT same players turn");
+    assertCustom(testG.numActions == G.numActions, "Number of actions same", "Number of actions changed" );
+    assertCustom(testG.numBuys == G.numBuys, "Number of buys same", "Number of buys changed" );
+    assertCustom(testG.playedCardCount == G.playedCardCount + 1, "1 card played", "Played card count incorrect");
+    assertCustom(testG.numPlayers == G.numPlayers, "Same number of players in the game", "Number of players in game changed");
 
     for(i = curse; i < NUM_CARDS; i++){
         if(testG.supplyCount[i] != G.supplyCount[i]){
@@ -65,7 +65,7 @@ int main() {
     //int newCards = 0;
     //int discarded = 1;
     //int xtraCoins = 0;
-    int shuffledCards = 0;
+    //int shuffledCards = 0;
     //int numBuys = 0;
     //int numActions =0;
 
