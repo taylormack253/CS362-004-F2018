b616df29cd19921dc591a841cf716f846d1201eb
William Taylor Mack <mackw@oregonstate.edu>
2018-11-19 19:25:37 -0800

    URLvalidatorIncorrect started

---
+++ projects/mackw/FinalProject/URLValidatorInCorrect/test/URLHelpers.java
@@ -0,0 +1,193 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class URLHelpers {
+
+    /*
+        Methods to generate random URL components
+     */
+
+    /*
+        Produces a random scheme of given size
+     */
+    public static String randomValidScheme(int size) {
+        Random rand = new Random();
+        String scheme = "";
+
+        int randomNum = rand.nextInt(51);
+        if (randomNum < 26) scheme += (char)(randomNum + 'a');
+        else scheme += (char)((randomNum - 26) + 'A');
+
+        for (int i = 1; i < size; i++) {
+            randomNum = rand.nextInt(64);
+            if (randomNum < 26) scheme += (char)(randomNum + 'a');
+            else if (randomNum < 52) scheme += (char)((randomNum - 26) + 'A');
+            else if (randomNum < 62) scheme += Integer.toString(randomNum - 52);
+            else if (randomNum == 62) scheme += '-';
+            else if (randomNum == 63) scheme += '+';
+            else scheme += '.';
+        }
+
+        return scheme;
+    }
+
+    /*
+        Produce a random IPv4 address
+     */
+    public static String randomValidIP() {
+        String ip = "";
+        Random rand = new Random();
+
+        ip += Integer.toString(rand.nextInt(255));
+
+        for (int i = 0; i < 3; i++) {
+            ip += '.' + Integer.toString(rand.nextInt(255));
+        }
+
+        return ip;
+    }
+
+    /*
+        Produce a random host name
+     */
+    public static String randomValidHost(int maxDomainLength, int numberOfDomains) {
+        String host = "";
+        Random rand = new Random();
+
+        for (int domainNum = 0; domainNum < numberOfDomains; domainNum++) {
+            int randomNum = rand.nextInt(51);
+            if (randomNum < 26) host += (char)(randomNum + 'a');
+            else host += (char)((randomNum - 26) + 'A');
+
+            int domainSize = rand.nextInt(maxDomainLength);
+            for (int domainChar = 0; domainChar < domainSize; domainChar++) {
+                randomNum = rand.nextInt(62);
+                if (randomNum < 26) host += (char)(randomNum + 'a');
+                else if (randomNum < 52) host += (char)((randomNum-26) + 'A');
+                else if (randomNum < 62) host += (char)((randomNum-52) + '0');
+                else host += '-';
+            }
+
+            host += '.';
+        }
+
+        // Add top level domain
+        int randomNum = rand.nextInt(TLDs.topLevelDomains.length - 1);
+        host += TLDs.topLevelDomains[randomNum];
+
+        return host;
+    }
+
+    public static String randomValidPortNumber() {
+        return randomInteger(0, 65535);
+    }
+
+    /*
+        Produces a random URL path. Specify the maximum length per segment and the number of segments
+     */
+    public static String randomValidPath(int maxSegmentLength, int numberOfSegments) {
+        String path = "";
+        Random rand = new Random();
+
+        for (int segmentNum = 0; segmentNum < numberOfSegments; segmentNum++) {
+            path += '/';
+
+            int segmentSize = rand.nextInt(maxSegmentLength-1) + 1;
+            for (int segmentChar = 0; segmentChar < segmentSize; segmentChar++) {
+                int randomNum = rand.nextInt(80);
+                if (randomNum < 26) path += (char)(randomNum + 'a');
+                else if (randomNum < 52) path += (char)((randomNum-26) + 'A');
+                else if (randomNum < 62) path += (char)((randomNum-52) + '0');
+                else if (randomNum == 62) path += '-';
+                else if (randomNum == 63) path += '_';
+                else if (randomNum == 64) path += '!';
+                else if (randomNum == 65) path += '~';
+                else if (randomNum == 66) path += '*';
+                else if (randomNum == 67) path += '\'';
+                else if (randomNum == 68) path += '(';
+                else if (randomNum == 69) path += ')';
+                else if (randomNum == 70) path += '%';
+                else if (randomNum == 71) path += ':';
+                else if (randomNum == 72) path += '@';
+                else if (randomNum == 73) path += '&';
+                else if (randomNum == 74) path += '=';
+                else if (randomNum == 75) path += '+';
+                else if (randomNum == 76) path += '$';
+                else if (randomNum == 77) path += ',';
+                else if (randomNum == 78) path += ';';
+                else if (randomNum == 79) path += '-';
+                else {
+                    if (path.charAt(path.length()-1) != '.') path += '.';
+                }
+            }
+        }
+
+        return path;
+    }
+
+    /*
+        Produces a random URL query of given length
+     */
+    public static String randomValidQuery(int size) {
+        String query = "";
+        Random rand = new Random();
+
+        if (size > 0) query += '?';
+
+        for (int i = 1; i < size; i++) {
+            int randomNum = rand.nextInt(81);
+            if (randomNum < 26) query += (char)(randomNum + 'a');
+            else if (randomNum < 52) query += (char)((randomNum-26) + 'A');
+            else if (randomNum < 62) query += (char)((randomNum-52) + '0');
+            else if (randomNum == 62) query += '-';
+            else if (randomNum == 63) query += '_';
+            else if (randomNum == 64) query += '!';
+            else if (randomNum == 65) query += '~';
+            else if (randomNum == 66) query += '*';
+            else if (randomNum == 67) query += '\'';
+            else if (randomNum == 68) query += '(';
+            else if (randomNum == 69) query += ')';
+            else if (randomNum == 70) query += '%';
+            else if (randomNum == 71) query += ':';
+            else if (randomNum == 72) query += '@';
+            else if (randomNum == 73) query += '&';
+            else if (randomNum == 74) query += '=';
+            else if (randomNum == 75) query += '+';
+            else if (randomNum == 76) query += '$';
+            else if (randomNum == 77) query += ',';
+            else if (randomNum == 78) query += ';';
+            else if (randomNum == 79) query += '-';
+            else if (randomNum == 80) query += '/';
+            else query += '?';
+
+        }
+        return query;
+    }
+
+    /*
+        Produces a random string. Not guaranteed to be valid for any part of a URL
+     */
+    // ASCII characters from " " to "~"
+    public static String randomString(int size) {
+        String randString = "";
+        Random rand = new Random();
+
+        int randomNum;
+
+        for (int i = 0; i < size; i++) {
+            randomNum = rand.nextInt(126-32);
+            randString += (char)(randomNum + ' ');
+        }
+
+        return randString;
+    }
+
+    public static String randomInteger(int minimum, int maximum) {
+        String randInt = "";
+        Random rand = new Random();
+
+        int randomNum = rand.nextInt(maximum-minimum) + minimum;
+        randInt = Integer.toString(randomNum);
+        return randInt;
+    }
+}
--- projects/mackw/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/mackw/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -2,12 +2,6 @@
 
 import junit.framework.TestCase;
 
-//You can use this as a skeleton for your 3 different test approach
-//It is an optional to use this file, you can generate your own test file(s) to test the target function!
-// Again, it is up to you to use this file or not!
-
-
-
 
 
 public class UrlValidatorTest extends TestCase {
@@ -21,26 +15,52 @@ public class UrlValidatorTest extends TestCase {
    
    public void testManualTest()
    {
-//You can use this function to implement your manual testing	   
+     //You can use this function to implement your manual testing	   
 	   
    }
    
    
    public void testYourFirstPartition()
    {
-	 //You can use this function to implement your First Partition testing	   
+	  //You can use this function to implement your First Partition testing	   
 
    }
    
    public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
+		//You can use this function to implement your Second Partition testing	   
 
    }
+   
    //You need to create more test cases for your Partitions if you need to 
    
    public void testIsValid()
    {
-	   //You can use this function for programming based testing
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES );
+      Random rand = new Random();
+
+      for (int i = 0; i < 10; i++) {
+         // set random scheme
+         //String URL = URLHelpers.randomValidScheme(5);
+         // lets start with just http
+         String URL = "http://";
+
+         // Generate random authority
+         URL += URLHelpers.randomValidHost(rand.nextInt(20)+1, rand.nextInt(5)+1);
+
+
+         // Generate random port number
+         URL += ":" + URLHelpers.randomValidPortNumber();
+         
+
+         // Generate random valid path with 0,1 or 2 segment
+         URL += URLHelpers.randomValidPath(rand.nextInt(10)+2, rand.nextInt(3));
+
+
+         // Generate random valid query
+         URL += URLHelpers.randomValidQuery(rand.nextInt(30));
+
+         // print URL
+         System.out.println(URL);
 
    }
    
