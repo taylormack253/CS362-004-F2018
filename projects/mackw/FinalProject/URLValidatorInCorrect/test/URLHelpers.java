17ba7b288ee5721267ad5d31bc64073f1b3ef220
William Taylor Mack <mackw@oregonstate.edu>
2018-11-20 20:21:38 -0800

    added manual testing and assertionis

--- projects/mackw/FinalProject/URLValidatorInCorrect/test/URLHelpers.java
+++ projects/mackw/FinalProject/URLValidatorInCorrect/test/URLHelpers.java
@@ -3,7 +3,281 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class URLHelpers {
+	
+   public static boolean isURLValid(String url) {
+       // Must contain :// after scheme
+       if (!url.contains("://")) return false;
 
+       // # is the end of the URL. Don't check anything after it
+       if (url.contains("#")) {
+           url = url.substring(0, url.indexOf('#'));
+       }
+
+       // Check scheme before ://
+       String scheme = url.substring(0, url.indexOf("://"));
+       if (!isSchemeValid(scheme)) return false;
+
+       url = url.substring(url.indexOf("://")+3);
+
+       // Check authority (before / or ?)
+       String authority = url;
+       if (authority.contains("?")) {
+           authority = authority.substring(0, authority.indexOf("?"));
+       }
+       if (authority.contains("/")) authority = authority.substring(0, authority.indexOf("/"));
+       if (!isAuthorityValid(authority)) return false;
+
+       // Check path if it exists
+       // For path to exist, there must be a "/" before any "?"
+       int pathStartIndex, pathEndIndex;
+       if (url.contains("/")) {
+           if (url.contains("?")) {
+               if (url.indexOf("/") < url.indexOf("?")) {
+                   pathStartIndex = url.indexOf("/");
+                   pathEndIndex = url.indexOf("?");
+               } else { // No path, ? before /
+                   pathStartIndex = 0;
+                   pathEndIndex = 0;
+               }
+           } else { // No ?, so the rest of the url is the path
+               pathStartIndex = url.indexOf("/");
+               pathEndIndex = url.length();
+           }
+       } else { // No /, no path
+           pathStartIndex = 0;
+           pathEndIndex = 0;
+       }
+       String path = url.substring(pathStartIndex, pathEndIndex);
+       if (!isPathValid(path)) return false;
+
+       // Validate query if there is one
+       if (url.contains("?")) {
+           String query = url.substring(url.indexOf("?"));
+           if (!isQueryValid(query)) return false;
+       }
+
+       // Else URL is valid
+       return true;
+   }
+
+   /*
+       Checks that a given scheme is valid
+    */
+   public static boolean isSchemeValid(String scheme) {
+       // Must have length at least 1
+       if (scheme.length() == 0) return false;
+
+       // Must begin with a letter
+       if (!Character.isLetter(scheme.charAt(0))) return false;
+
+       // Every character must be a letter, number, +, -, or .
+       // according to RFC2396 Appendix A
+       for (int strIndex = 1; strIndex < scheme.length(); strIndex++) {
+           if (!(
+                   Character.isLetter(scheme.charAt(strIndex)) ||
+                           Character.isDigit(scheme.charAt(strIndex)) ||
+                           scheme.charAt(strIndex) == '+' ||
+                           scheme.charAt(strIndex) == '-' ||
+                           scheme.charAt(strIndex) == '.')) {
+               return false;
+           }
+       }
+
+       // Else it is a valid scheme
+       return true;
+   }
+
+   /*
+       Checks that a given authority is valid. Can be a host name
+       or IPv4 address
+    */
+   public static boolean isAuthorityValid(String authority) {
+       if (isIPValid(authority)) return true;
+       else if (isHostValid(authority)) return true;
+       else return false;
+   }
+
+   /*
+       Checks if a given string is a valid IPv4 address
+    */
+   public static boolean isIPValid(String ip) {
+
+       // If it has a port, check that first
+       if (ip.contains(":")) {
+           String port = ip.substring(ip.indexOf(":")+1);
+           if (!isPortValid(port)) return false;
+
+           ip = ip.substring(0, ip.indexOf(":"));
+       }
+
+       int bytes[] = {-1, -1, -1, -1}; // The 4 bytes that make up IP address
+       try {
+           // 3 times, parse out a number and cut off everything through the period
+           for (int i = 0; i < 3; i++) {
+               bytes[i] = Integer.parseInt(ip.substring(0, ip.indexOf(".")));
+               ip = ip.substring(ip.indexOf(".")+1);
+           }
+           // last time, just parse the remaining number
+           bytes[3] = Integer.parseInt(ip);
+       } catch (Exception e) {
+           // Any exception in parsing or substings means it was not valid
+           return false;
+       }
+
+       // Each byte must not exceed the size of 1 byte
+       for (int i = 0; i < 4; i ++) {
+           if (bytes[i] < 0 || bytes[i] > 255) return false;
+       }
+
+       // Else IP is valid
+       return true;
+   }
+
+   /*
+       Checks that a given string can be a valid host name. Checks the top level domain against a list of registered TLDs
+    */
+   public static boolean isHostValid(String host) {
+       // First check the port if there is one
+       if (host.contains(":")) {
+           String port = host.substring(host.indexOf(":")+1);
+           if (!isPortValid(port)) return false;
+
+           host = host.substring(0, host.indexOf(":"));
+       }
+
+       // Cut off . at the end
+       if (host.length() >= 1) {
+           if (host.charAt(host.length() - 1) == '.') host = host.substring(0, host.length() - 1);
+       }
+
+       // If it is a local TDL, it is exempt from the rules below
+       if (host.equalsIgnoreCase("localhost") || host.equalsIgnoreCase("localdomain")) return true;
+
+       // There must be a top level domain and at least one lower level domain, separated by "."
+       // Domains must start with a letter or number and can have "-" in the middle
+       // Top level domains must belong to a list of existing TLD's
+
+       // Must contain "."
+       if (!host.contains(".")) return false;
+
+       while (host.contains(".")) {
+           String domain = host.substring(0,host.indexOf("."));
+           host = host.substring(host.indexOf(".")+1);
+
+           // domain must have at least 1 character
+           if (domain.length() == 0) return false;
+
+           // Each character must be alphanum or "-". First and last cannot be "-"
+           if (domain.charAt(0) == '-' || domain.charAt(domain.length()-1) == '-') return false;
+
+           for (int strIndex = 0; strIndex < domain.length(); strIndex++) {
+               if (!(
+                       Character.isLetter(domain.charAt(strIndex)) ||
+                               Character.isDigit(domain.charAt(strIndex)) ||
+                               domain.charAt(strIndex) == '-')) {
+                   return false;
+               }
+           }
+       }
+
+       // Top level domain must belong to the list of existing top level domains
+       // DNS is case insensitive
+       host = host.toLowerCase();
+       if (Arrays.binarySearch(TLDs.topLevelDomains, host) < 0) return false;
+
+       return true;
+   }
+
+   /*
+       Checks that a given port number is valid (between 0 and 65535)
+    */
+   public static boolean isPortValid(String port) {
+       // if port is blank, it is accepted
+       if (port.length() == 0) return true;
+
+       int portNum = -1;
+       // Port must be an integer
+       try {
+           portNum = Integer.parseInt(port);
+       } catch (NumberFormatException e) {
+           return false;
+       }
+
+       // Must be between 0 and 65535
+       if (portNum < 0 || portNum > 65535) return false;
+
+       // Else it is a valid port
+       return true;
+   }
+
+   /*
+       Checks the characters in a string to see if it can be a URL path
+    */
+   public static boolean isPathValid(String path) {
+       // If there is a path, it has to start with a slash (/)
+       if (path.length() > 0) {
+           if (path.charAt(0) != '/') return false;
+       }
+
+       // Every character in the path must belong to the list below
+       // Unreserved, escaped (%), or ":", "@", "&", "=", "+", "$", ",", ";"
+       for (int strIndex = 0; strIndex < path.length(); strIndex++) {
+           if (!(
+                   Character.isLetter(path.charAt(strIndex)) ||
+                           Character.isDigit(path.charAt(strIndex)) ||
+                           path.charAt(strIndex) == '-' ||
+                           path.charAt(strIndex) == '_' ||
+                           path.charAt(strIndex) == '.' ||
+                           path.charAt(strIndex) == '!' ||
+                           path.charAt(strIndex) == '~' ||
+                           path.charAt(strIndex) == '*' ||
+                           path.charAt(strIndex) == '\'' ||
+                           path.charAt(strIndex) == '(' ||
+                           path.charAt(strIndex) == ')' ||
+                           path.charAt(strIndex) == '%' ||
+                           path.charAt(strIndex) == ':' ||
+                           path.charAt(strIndex) == '@' ||
+                           path.charAt(strIndex) == '&' ||
+                           path.charAt(strIndex) == '=' ||
+                           path.charAt(strIndex) == '+' ||
+                           path.charAt(strIndex) == '$' ||
+                           path.charAt(strIndex) == ',' ||
+                           path.charAt(strIndex) == ';' ||
+                           path.charAt(strIndex) == '/')) {
+               return false;
+           }
+
+           // No consecutive slash or period
+           if (path.charAt(strIndex) == '/' && path.length() > strIndex+1) {
+               if (path.charAt(strIndex + 1) == '/') return false;
+           }
+           if (path.charAt(strIndex) == '.' && path.length() > strIndex+1) {
+               if (path.charAt(strIndex + 1) == '.') return false;
+           }
+       }
+
+       // Else path is valid
+       return true;
+   }
+
+   /*
+       Checks the characters in a string to see if it can be a URL query
+    */
+   public static boolean isQueryValid(String query) {
+       // Query must start with ?
+       if (query.length() > 0) {
+           if (query.charAt(0) != '?') return false;
+       }
+
+       // Characters before pound sign must not be whitespace
+       // Characters after pound sign can be whitespace
+       for (int strIndex = 0; strIndex < query.length(); strIndex++) {
+           if (Character.isWhitespace(query.charAt(strIndex))) return false;
+       }
+
+       // Else query is valid
+       return true;
+   }
     /*
         Methods to generate random URL components
      */
@@ -51,29 +325,31 @@ public class URLHelpers {
     /*
         Produce a random host name
      */
-    public static String randomValidHost(int maxDomainLength, int numberOfDomains) {
-        String host = "";
+    public static String randomValidHost(int maxDomainLength) {
+        String host = "www.";
         Random rand = new Random();
 
-        for (int domainNum = 0; domainNum < numberOfDomains; domainNum++) {
             int randomNum = rand.nextInt(51);
             if (randomNum < 26) host += (char)(randomNum + 'a');
             else host += (char)((randomNum - 26) + 'A');
 
             int domainSize = rand.nextInt(maxDomainLength);
             for (int domainChar = 0; domainChar < domainSize; domainChar++) {
-                randomNum = rand.nextInt(62);
-                if (randomNum < 26) host += (char)(randomNum + 'a');
-                else if (randomNum < 52) host += (char)((randomNum-26) + 'A');
-                else if (randomNum < 62) host += (char)((randomNum-52) + '0');
-                else host += '-';
-            }
+            randomNum = rand.nextInt(62);
+            // lowercase letters
+            if (randomNum < 26) host += (char)(randomNum + 'a');
+            // uppercase letters
+            else if (randomNum < 52) host += (char)((randomNum-26) + 'A');
+            // valid symbols
+            else if (randomNum < 62) host += (char)((randomNum-52) + '0');
+            else host += '-';
+         }
 
-            host += '.';
-        }
+         host += '.';
+    
 
         // Add top level domain
-        int randomNum = rand.nextInt(TLDs.topLevelDomains.length - 1);
+        randomNum = rand.nextInt(TLDs.topLevelDomains.length - 1);
         host += TLDs.topLevelDomains[randomNum];
 
         return host;
--- projects/mackw/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/mackw/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -6,6 +6,7 @@ import src.UrlValidator;
 import java.util.Random;
 
 
+
 public class UrlValidatorTest extends TestCase {
 
 
@@ -13,12 +14,94 @@ public class UrlValidatorTest extends TestCase {
       super(testName);
    }
 
+   public void customAssert(String statement, boolean boolOne, boolean boolTwo)
+   {
+      String assertionError = null;
+      try {
+         assertEquals(boolOne,boolTwo);
+      }
+      catch (AssertionError ae) {
+         assertionError = ae.toString();
+      }
+
+      if(assertionError != null)
+         System.out.printf("%s %s %n", statement, assertionError);
+   }
    
    
    public void testManualTest()
    {
-     //You can use this function to implement your manual testing	   
+	   System.out.println("Manual Test....");
 	   
+	   String[] trueURLsTwoSlash = {
+	              "http://localhost/", // Typical with slash
+	              "http://machine/", // local address
+	              "http://localhost:8000", // with ports
+	              "ftp://localhost:8000",
+	              "http://machine:0",
+	              "http://www.google.com/test//testfile",
+	              "ftp://www.google.com/test//testfile",
+	              "ftp://www.google.com/file1//test2//test3"
+	      };
+
+	      // Insert the manual URls that we are going to test
+	      String[] trueURLS = {"http://www.google.com", //valid url
+	              "http://www.google.com/", // valid with slash
+	              "http://0.0.0.0/", // local address
+	              "http://www.google.com/test", // path
+	              "http://stackoverflow.com/", // No Domain
+	              "http://www.google.com/test/test/test/test/test/test/testFile", // long path
+	              "http://www.google.com/test?action=view", // query
+	              "http://www.google.com/#/",// path with symbol
+	              "http://www.google.com/23/", // path with number
+	              "http://www.google.ceo", // different TLD
+	              "http://www.google.food",
+	              "http://www.google.com:0", // ports
+	              "http://www.google.com:6000"
+	      };
+
+	      // List all false URLs
+	      String[] falseURLS = {"http://www.google.cmx/", // bad TLD
+	              "http://www.google.aaab", // bad TLD
+	              "http://www.google.jdifj", // bad TLD case
+	              "ftp://www.google.jdifj", // bad TLD cases
+
+	              "htp:/www.google.com/", // Incorrect Scheme Variations
+	              "http:/www.google.com/",
+	              "http:/www.google.com/#", //address with incorrect path option",
+	              "255.255.255.255", // no Scheme
+	              "http://256.256.256.256", // Impossible IP address
+	              "ftp://256.256.256.256",
+	              "http://www.google.com/..//file", // dots in file
+	              "http://www.google.com:-1", // bad Port
+	              "http://www.google.com:65a",
+	              "http://www.google.com/test//testfile", // try two slashes
+	              "http://localhost/", // Typical with slash
+	              "http://machine/", // local address
+	              "http://localhost:8000", // with ports
+	              "ftp://localhost:8000",
+	              "http://machine:0",
+	              "http://www.google.com/test//testfile",
+	              "ftp://www.google.com/test//testfile",
+	              "ftp://www.google.com/file1//test2//test3"
+	      };
+	   
+	      // setup default UrlValudator with "http,https,ftp" as valid schemes
+	      UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_2_SLASHES);
+	   
+	   	  // Run through the true URLs
+	      for(int i = 0; i < trueURLS.length; i++)
+	      {
+	         customAssert(trueURLS[i], true, urlVal.isValid(trueURLS[i]));
+	      }
+
+	      // Run through the false URLs
+	      for(int i = 0; i < falseURLS.length; i++)
+	      {
+	         customAssert(falseURLS[i], false, urlVal.isValid(falseURLS[i]));
+	      }
+	     
+	      
    }
    
    
@@ -47,7 +130,7 @@ public class UrlValidatorTest extends TestCase {
          String URL = "http://";
 
          // Generate random authority
-         URL += URLHelpers.randomValidHost(rand.nextInt(20)+1, rand.nextInt(5)+1);
+         URL += URLHelpers.randomValidHost(rand.nextInt(20)+1);
 
 
          // Generate random port number
@@ -62,8 +145,21 @@ public class UrlValidatorTest extends TestCase {
          URL += URLHelpers.randomValidQuery(rand.nextInt(30));
 
          // print URL
-         System.out.println(URL);
+         System.out.printf("URL # %d: %s %n",i + 1, URL);
 
+         
+         try {
+        	 // get expected value from URLHelpers
+             boolean expected = URLHelpers.isURLValid(URL);
+             // test URL using URLValidator
+             boolean actual = urlVal.isValid(URL);
+             // check results match
+             customAssert(URL, expected, actual);
+             //if (expected != actual) deltaDebug(URL);
+          } catch (Exception e) {
+             //e.printStackTrace();
+             System.out.printf("Caused exception: %s %n",URL);
+          }
    }
    
 
